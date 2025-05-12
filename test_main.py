cddce70113dd10445fbc568c729238e6dafc5b7c
Sylvain Zoogones <140527091+Sylvain-Z@users.noreply.github.com>
2025-05-12 17:44:11 +0200

    fix pipeline

--- test_main.py
+++ test_main.py
@@ -4,7 +4,6 @@ from main import hello
 def test_hello():
     assert hello('GitHub Actions') == "Hello, GitHub Actions!"
     
-    
 
 def test_hello_custom_name():
     assert hello('Sylvain') == "Hello, Sylvain!"
