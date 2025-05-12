52b92eec29e8a523f3518a1dae43f90e8e4e7f69
Sylvain Zoogones <140527091+Sylvain-Z@users.noreply.github.com>
2025-05-12 17:41:55 +0200

    add custom test

--- test_main.py
+++ test_main.py
@@ -3,3 +3,8 @@ from main import hello
 
 def test_hello():
     assert hello('GitHub Actions') == "Hello, GitHub Actions!"
+    
+    
+
+def test_hello_custom_name():
+    assert hello('Sylvain') == "Hello, Sylvain!"
